7652ba876787b55133dec8afbd59cb16f209e9a6
arc <2336429253@qq.com>
2024-03-23 20:58:30 +0800

    🧪 test(可调度性): 添加对全局EDF充分测试的单元测试
    测试可调度性的充分性

--- tests/test_schedulability.py
+++ tests/test_schedulability.py
@@ -1,16 +1,60 @@
 import unittest
 
 from simRT import Schedulability
+from simRT.core.model import Simulator
+from simRT.core.processor import PlatformInfo
 from simRT.core.task import PeriodicTask, TaskInfo
+from simRT.generator import TaskGenerator, Taskset
 
 
 class TestSchedulability(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.task_gen = TaskGenerator(
+            task_type=PeriodicTask,
+            period_bound=(5, 20),
+            platform_info=[1, 0.5],
+            implicit_deadline=True,
+        )
+        self.num_task = len(self.task_gen.platform_info.speed_list) * 2 + 1
+
     def test_G_EDF_sufficient_test_case1(self):
         triplets = [(2, 10, 10), (1, 10, 10), (10, 11, 11)]
         taskinfos = [
             TaskInfo(i, PeriodicTask, *triplet) for i, triplet in enumerate(triplets)
         ]
         schedulability = Schedulability.G_EDF_sufficient_test(
-            Gamma=taskinfos, speed_list=[1, 0.5]
+            Gamma=taskinfos, processors=PlatformInfo([1, 0.5])
         )
         print(schedulability)
+
+    def test_G_EDF_sufficient_test_case2(self):
+        num_task_set = 10
+        tasksets = self.task_gen.generate_taskset(
+            system_utilization=0.8, num_task=self.num_task, num_task_set=num_task_set
+        )
+        success = 0
+        sim_success = 0
+        sim_fail = 0
+        for i, taskset in enumerate(tasksets):
+            sim = Simulator(taskset, self.task_gen.platform_info)
+            sim_schedulability = sim.run(show_progress=True)
+            schedulability = Schedulability.G_EDF_sufficient_test(
+                Gamma=taskset, processors=self.task_gen.platform_info
+            )
+
+            # print(f"[{i}] test:{schedulability}, sim:{sim_schedulability}")
+            if schedulability is True:
+                success += 1
+                self.assertTrue(sim_schedulability)
+
+            if sim_schedulability is False:
+                sim_fail += 1
+                self.assertFalse(schedulability)
+
+            if sim_schedulability is True:
+                sim_success += 1
+
+        self.assertEqual(sim_fail + sim_success, num_task_set)
+        # print(f"sim success rate: {sim_success/num_task_set}")
+        # print(f"success rate: {success/num_task_set}")
