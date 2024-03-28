2d5aca2a010b850f55288cbff44034310bd6badd
polyarc <2336429253@qq.com>
2024-03-28 20:07:54 +0800

    🎈 perf(超图生成器): 若充分测试为真，则必定满足充要条件，不需要进行模拟

--- simRT/generator/hypergraph_generator.py
+++ simRT/generator/hypergraph_generator.py
@@ -141,11 +141,17 @@ class TaskHypergraphGenerator:
         db_path: Optional[Path] = None,
         cutoff: Optional[SimTime] = None,
     ) -> tuple[Taskset, bool, bool]:
-        sim = Simulator(taskset, platform_info)
-        # 可调度性充要条件
-        ns_result = sim.run(until=cutoff)
         # 可调度性充分非必要条件
         sufficient = Schedulability.G_EDF_sufficient_test(taskset, platform_info)
+
+        # 如果充分测试为可调度，则必定满足充要条件
+        if sufficient is False:
+            # 可调度性充要条件
+            sim = Simulator(taskset, platform_info)
+            ns_result = sim.run(until=cutoff)
+        else:
+            ns_result = True
+
         if db_path is not None:
             task_db = TaskStorage(db_path)
             taskset_u = sum(task.utilization for task in taskset)
